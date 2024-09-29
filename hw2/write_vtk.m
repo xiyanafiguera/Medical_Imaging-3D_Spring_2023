e6a6d97ee531f25298d65cf700ac4eebeeab4b40
Xiyana Figuera <112554275+xiyanafiguera@users.noreply.github.com>
2024-09-29 19:09:50 +0900

    Add hw2 files

---
+++ hw2/write_vtk.m
@@ -0,0 +1,9 @@
+function write_vtk(fn,v,f)
+    fp = fopen(fn,'w');
+    fprintf(fp, '# vtk DataFile Version 3.0\nvtk output\nASCII\nDATASET POLYDATA\n');
+    fprintf(fp, 'POINTS %d float\n', size(v, 1));
+    fprintf(fp, '%f %f %f\n', v');
+    fprintf(fp, 'POLYGONS %d %d\n', size(f, 1), size(f, 1) * 4);
+    fprintf(fp, '3 %d %d %d\n', f');
+    fclose(fp);
+end
